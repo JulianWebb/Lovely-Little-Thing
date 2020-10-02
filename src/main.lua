9a09a79f99b279c1a4364b89fad41e43b4087202
Julian Webb <git@julianwebb.ca>
2020-10-02 00:39:45 -0700

    Fleshed out main.lua a bit

--- src/main.lua
+++ src/main.lua
@@ -1,1 +1,17 @@
 require 'init'
+
+local bgColor = hex.rgb("5500AA")
+
+local mouseCoords = Debug:addMessage('Mouse Coords: 0, 0')
+
+function love.update(dt)
+    Debug:update(dt)
+
+    local mouseX, mouseY = love.mouse.getPosition()
+    Debug:updateMessage(mouseCoords, 'Mouse Coords: ' .. mouseX .. ', ' .. mouseY)
+end
+
+function love.draw()
+    Debug:draw()
+    love.graphics.setBackgroundColor(bgColor)
+end
